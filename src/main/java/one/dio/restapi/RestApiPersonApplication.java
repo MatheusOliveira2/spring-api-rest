5a9fa7bae536da4332a9abe00a3fe782d82ababc
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-07-11 14:25:08 -0300

    first commit

---
+++ src/main/java/one/dio/restapi/RestApiPersonApplication.java
@@ -0,0 +1,13 @@
+package one.dio.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiPersonApplication.class, args);
+	}
+
+}
